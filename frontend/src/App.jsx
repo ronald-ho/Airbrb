0457294de06c3eca1e1388266ce2026e1609ded4
ronald-ho <ronaldho1202@gmail.com>
2023-10-29 13:17:40 +1100

    commited first pages and file structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import AllListings from './pages/AllListings';
+import HostedListings from './pages/HostedListings';
+import EditListing from './pages/EditListing';
+import ViewListing from './pages/ViewListing';
+import BookingHistory from './pages/BookingHistory';
 
 function App () {
   return (
-    <>Let&apos;s go!</>
-  );
+    <Router>
+      <Switch>
+        <Route path="/" exact component={AllListings}/>
+        <Route path="/login" exact component={Login}/>
+        <Route path="/register" exact component={Register}/>
+        <Route path="/my-listings" exact component={HostedListings}/>
+        <Route path="/my-listings/edit/:id" exact component={EditListing}/>
+        <Route path="/listings" exact component={AllListings}/>
+        <Route path="/listing/:id" exact component={ViewListing}/>
+        <Route path="/booking-history/:id" exact component={BookingHistory}/>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
