5453aa72fd206492598624a2f91c485b37b4a3b5
ronald-ho <ronaldho1202@gmail.com>
2023-11-07 21:22:03 +1100

    minor bug fixes

--- frontend/src/pages/EditListing.jsx
+++ frontend/src/pages/EditListing.jsx
@@ -121,13 +121,8 @@ function EditListing () {
     try {
       const response = await updateListing(listingId, updatedListing);
 
-      if (response.success) {
-        console.log('Updated listing: ', response);
-        toast({ title: 'Listing updated successfully', status: 'success', duration: 3000, isClosable: true, })
-      } else {
-        setError(response.error || 'Error updating listing');
-        toast({ title: error, status: 'error', duration: 3000, isClosable: true, })
-      }
+      console.log('Updated listing: ', response);
+      toast({ title: 'Listing updated successfully', status: 'success', duration: 3000, isClosable: true, })
     } catch (err) {
       setError(error.message);
       toast({ title: error, status: 'error', duration: 3000, isClosable: true, })
