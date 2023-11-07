3e66c941bf556e61fa904217c13c79e3810479cd
G Huang <ghuang582@gmail.com>
2023-11-07 20:59:43 +1100

    Fixed conflicts again

--- frontend/src/components/listingPreview.jsx
+++ frontend/src/components/listingPreview.jsx
@@ -3,12 +3,8 @@ import { Badge, Box, Image } from '@chakra-ui/react'
 import { StarIcon } from '@chakra-ui/icons';
 import { Link } from 'react-router-dom';
 
-<<<<<<< HEAD
 function ListingPreview (listing) {
   // Get review information
-=======
-function listingPreview (listing) {
->>>>>>> 22ee39101905cf5f83eb0336abfd6120ffe835c6
   let totalRatings = 0;
 
   for (const review of listing.reviews) {
@@ -21,7 +17,6 @@ function listingPreview (listing) {
   const metadata = listing.metadata;
 
   return (
-<<<<<<< HEAD
     <Link to={`/listing/${listing.listingId}`}>
       <Box>
         <Image src={listing.thumbnail} />
@@ -47,48 +42,6 @@ function listingPreview (listing) {
             ml='2'
           >
             {metadata.bedrooms} beds &bull; {metadata.bathrooms} baths
-=======
-    <Box>
-      <Image src={listing.thumbnail}/>
-      <Box p='6'>
-        <Box display='flex' alignItems='baseline'>
-          <Badge borderRadius='full' px='2' colorScheme='teal'>
-            {metadata.propertyType}
-          </Badge>
-          {
-            listing.bookingStatus && listing.bookingStatus !== 'zzz'
-              ? (<Badge borderRadius='full' px='2' colorScheme='red'>
-                {listing.bookingStatus}
-              </Badge>)
-              : null
-          }
-        </Box>
-        <Box
-          color='gray.500'
-          fontWeight='semibold'
-          letterSpacing='wide'
-          fontSize='xs'
-          textTransform='uppercase'
-          ml='2'
-        >
-          {metadata.bedrooms.length} beds &bull; {metadata.bathrooms} baths
-        </Box>
-
-        <Box
-          mt='1'
-          fontWeight='semibold'
-          as='h4'
-          lineHeight='tight'
-          isTruncated
-        >
-          {listing.title}
-        </Box>
-
-        <Box>
-          ${listing.price}
-          <Box as='span' color='gray.600' fontSize='sm'>
-            / wk
->>>>>>> 22ee39101905cf5f83eb0336abfd6120ffe835c6
           </Box>
 
           <Box
