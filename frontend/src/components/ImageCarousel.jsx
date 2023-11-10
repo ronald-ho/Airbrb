f6e2661a0e212a879020eb40c8a53fe640cfa45b
G Huang <ghuang582@gmail.com>
2023-11-11 03:53:50 +1100

    Buttons show when hovering over image

--- frontend/src/components/ImageCarousel.jsx
+++ frontend/src/components/ImageCarousel.jsx
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Image, IconButton, AspectRatio } from '@chakra-ui/react';
 import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons';
 
 function ImageCarousel ({ thumbnail, allImages, index, handlePrev, handleNext }) {
+  const [style, setStyle] = useState(false);
+
+  const toggleStyle = () => {
+    setStyle(!style);
+  }
+
   return (
-    <Box position='relative'>
-    <IconButton
-      position='absolute'
-      top='50%'
-      transform="translateY(-50%)"
-      icon={<ArrowLeftIcon />}
-      onClick={handlePrev}
-      zIndex={100} // Button randomly disappeared after using aspect ratio
-    />
-    <AspectRatio ratio={4 / 3}>
-      <Image src={!allImages ? thumbnail : allImages[index]} width='100%' />
-    </AspectRatio>
-    <IconButton
-      position='absolute'
-      top='50%'
-      right='0'
-      transform="translateY(-50%)"
-      icon={<ArrowRightIcon />}
-      onClick={handleNext}
-    />
-  </Box>
+    <Box
+      position='relative'
+      onMouseEnter={toggleStyle}
+      onMouseLeave={toggleStyle}
+    >
+      <IconButton
+        position='absolute'
+        top='50%'
+        left='1'
+        transform="translateY(-50%)"
+        isRound='true'
+        size='sm'
+        display={ style ? 'block' : 'none' }
+        icon={<ArrowLeftIcon />}
+        onClick={handlePrev}
+        zIndex={100}
+      />
+      <AspectRatio ratio={4 / 3}>
+        <Image src={!allImages ? thumbnail : allImages[index]} width='100%' />
+      </AspectRatio>
+      <IconButton
+        position='absolute'
+        top='50%'
+        right='1'
+        transform="translateY(-50%)"
+        isRound='true'
+        size='sm'
+        display={ style ? 'block' : 'none' }
+        icon={<ArrowRightIcon />}
+        onClick={handleNext}
+      />
+    </Box>
   );
 }
 
