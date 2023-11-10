a6e778fcd90031c66ef879c18238e327fafb2a6e
G Huang <ghuang582@gmail.com>
2023-11-11 02:28:29 +1100

    Turned an element into reusable component

---
+++ frontend/src/components/StarRating.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StarIcon } from '@chakra-ui/icons';
+
+function StarRating ({ rating }) {
+  return (
+    Array(5).fill('')
+      .map((_, i) => (
+        <StarIcon
+          key={i}
+          color={i < rating ? 'black.400' : 'gray.300'}
+        />
+      ))
+  )
+}
+
+export default StarRating;
