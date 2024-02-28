0a90e72d740c884ff5b1ecab41fbe52d58ed4580
Ronald Ho <66163786+ronald-ho@users.noreply.github.com>
2024-02-28 12:13:24 +1100

    Update api.jsx to take in process.env

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import config from '../config';
 
-const API_BASE_URL = config.API_BASE_URL;
+const API_BASE_URL = process.env.API_BASE_URL;
 
 axios.interceptors.request.use(
   (config) => {
